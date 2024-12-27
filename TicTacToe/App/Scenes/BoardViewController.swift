8e0453ea8f252cb87a66d44e118c41874ae9fd07
Diggo Silva <rdiggosilva@gmail.com>
2024-12-27 10:00:20 -0300

    Removed .DS_Store from repository

---
+++ TicTacToe/App/Scenes/BoardViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class BoardViewController: UIViewController {
+    
+    let boardView = BoardView()
+    
+    override func loadView() {
+        super.loadView()
+        view = boardView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
