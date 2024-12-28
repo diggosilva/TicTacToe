c26898f606444c8dee89aba9b1189cd83c71df69
Diggo Silva <rdiggosilva@gmail.com>
2024-12-28 11:18:15 -0300

    Created BoardViewController

--- TicTacToe/App/Scenes/BoardViewController.swift
+++ TicTacToe/App/Scenes/BoardViewController.swift
@@ -1,3 +1,10 @@
+//
+//  BoardViewController.swift
+//  TicTacToe
+//
+//  Created by Diggo Silva on 27/12/24.
+//
+
 import UIKit
 
 class BoardViewController: UIViewController {
@@ -11,6 +18,54 @@ class BoardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setNavBar()
+        setDelegatesAndDataSources()
+    }
+    
+    private func setNavBar() {
+        title = "Jogo da velha"
+    }
+    
+    private func setDelegatesAndDataSources() {
+        boardView.delegate = self
+    }
+}
+
+extension BoardViewController: BoardViewDelegate {
+    func buttonTapped(_ sender: UIButton) {
+        boardView.emptySpace -= 1
+        boardView.labelTurn.text = boardView.isXTurn ? "É a vez de ⭕" : "É a vez de ❌"
+        sender.setTitle(boardView.isXTurn ? "❌" : "⭕", for: .normal)
+        sender.isEnabled = false
+        boardView.isXTurn.toggle()
+        boardView.checkWinner(sender)
+    }
+    
+    func checkWinner(_ sender: UIButton) {
+        if boardView.buttonA1.currentTitle == sender.currentTitle && boardView.buttonA2.currentTitle == sender.currentTitle && boardView.buttonA3.currentTitle == sender.currentTitle ||
+           boardView.buttonB1.currentTitle == sender.currentTitle && boardView.buttonB2.currentTitle == sender.currentTitle && boardView.buttonB3.currentTitle == sender.currentTitle ||
+           boardView.buttonC1.currentTitle == sender.currentTitle && boardView.buttonC2.currentTitle == sender.currentTitle && boardView.buttonC3.currentTitle == sender.currentTitle  {
+               boardView.buttons.forEach({ $0.isEnabled = false })
+               print("DEBUG: HORIZONTAL WIN")
+        }
+        else if boardView.buttonA1.currentTitle == sender.currentTitle && boardView.buttonB1.currentTitle == sender.currentTitle && boardView.buttonC1.currentTitle == sender.currentTitle ||
+                boardView.buttonA2.currentTitle == sender.currentTitle && boardView.buttonB2.currentTitle == sender.currentTitle && boardView.buttonC2.currentTitle == sender.currentTitle ||
+                boardView.buttonA3.currentTitle == sender.currentTitle && boardView.buttonB3.currentTitle == sender.currentTitle && boardView.buttonC3.currentTitle == sender.currentTitle  {
+            boardView.buttons.forEach({ $0.isEnabled = false })
+            print("DEBUG: VERTICAL WIN")
+        }
+        else if boardView.buttonA1.currentTitle == sender.currentTitle && boardView.buttonB2.currentTitle == sender.currentTitle && boardView.buttonC3.currentTitle == sender.currentTitle ||
+                boardView.buttonA3.currentTitle == sender.currentTitle && boardView.buttonB2.currentTitle == sender.currentTitle && boardView.buttonC1.currentTitle == sender.currentTitle {
+            boardView.buttons.forEach({ $0.isEnabled = false })
+            print("DEBUG: DIAGONAL WIN")
+        } else if boardView.emptySpace == 0 {
+            print("DEBUG: EMPATE")
+        }
+    }
+    
+    func resetGame() {
+        boardView.buttons.forEach({ $0.setTitle("", for: .normal) })
+        boardView.buttons.forEach({ $0.isEnabled = true })
+        boardView.emptySpace = 9
     }
 }
