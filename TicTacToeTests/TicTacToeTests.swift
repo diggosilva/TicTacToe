9a14a988633a287cd973c0c08f0937873df5b5cb
Diggo Silva <rdiggosilva@gmail.com>
2024-12-31 10:28:08 -0300

    Starting tests for TicTacToe logic

---
+++ TicTacToeTests/TicTacToeTests.swift
@@ -0,0 +1,22 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToeTests
+//
+//  Created by Diggo Silva on 31/12/24.
+//
+
+import XCTest
+@testable import TicTacToe
+
+final class TicTacToeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+}
